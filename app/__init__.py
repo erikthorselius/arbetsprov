749a6f2f5a7eb3dd1aeccf4ba2df18cc64026eea
Erik Thorselius <erik.thorselius@gmail.com>
2018-02-28 18:11:42 +0100

    Endpoints for message api

--- app/__init__.py
+++ app/__init__.py
@@ -1,47 +1,53 @@
 from flask_api import FlaskAPI
 from instance.config import app_config
 from flask import request, jsonify, abort
+from app.models import MessageFactory
+from app.database import Database
 from pymongo import MongoClient
-from pprint import pprint
-
-client = MongoClient()
-db=client.admin
 
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    message_factory = MessageFactory(app.config["SECRET"])
+    db = Database(MongoClient(app.config["DATABASE_URI"]))
 
-    from app.models import Messages
-    @app.route('/messages/', methods=['POST', 'GET'])
-    def messages():
-        if request.method == "POST":
-            name = str(request.data.get('name', ''))
-            if name:
-                message = Messages(name=name)
-                message.save()
-                response = jsonify({
-                    'id': '123',
-                    'name': message.name,
-                })
-                response.status_code = 201
-                return response
+    @app.route('/messages/<user_id>/<message_id>', methods=['GET', 'DELETE'])
+    def message(user_id, message_id):
+        if request.method == "GET":
+            message = db.get_message(message_id)
+            if message != None:
+                return message
+            else:
+                abort(404)
         else:
-            # GET
-            messages = Messages.get_all()
-            results = []
+            count = db.delete_message(message_id)
+            if count == 1:
+                response = jsonify()
+                response.status_code = 204
+                return response
+            else:
+                abort(404)
 
-            for message in messages:
-                obj = {
-                    'id': message.id,
-                    'name': message.name,
-                    'date_created': message.date_created,
-                    'date_modified': message.date_modified
-                }
-                results.append(obj)
-            response = jsonify(results)
-            response.status_code = 200
+    @app.route('/messages/<user_id>', methods=['POST', 'GET'])
+    def messages(user_id):  # TODO Validate user_id
+        if request.method == "POST":
+            user_input = str(request.data.get('message', ''))  # TODO Validate user_input
+            message = message_factory.create(user_id, user_input)
+            id = db.save_message(message)
+            response = jsonify(id=id)
+            response.status_code = 201
             return response
+        else:
+            messages = db.get_messages(user_id)
+            if messages != 0:
+                return messages
+            else:
+                abort(404)  # Not sure if I should return 404 or 204
+
+    @app.route('/status', methods=['GET'])
+    def status():
+        return (jsonify(db.status()))
 
     return app
--- test_message_api.py
+++ test_message_api.py
@@ -1,26 +1,56 @@
 import unittest
-import os
-import json
 from app import create_app
+import json
+from pprint import pprint
+from urllib.parse import urljoin
 
-class BucketlistTestCase(unittest.TestCase):
-    """This class represents the message test case"""
 
+class MessageApiTestCase(unittest.TestCase):
     def setUp(self):
-        """Define test variables and initialize app."""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
-        self.message = {'name': 'Go to Borabora for vacation'}
-
+        self.message_data = 'message_data'
 
     def test_message_creation(self):
-        """Test API can create a message (POST request)"""
-        res = self.client().post('/messages/', data=self.message)
+        username = "username"
+        res = self.client().post('/messages/' + username, data={'message': self.message_data})
+        data = json.loads(res.data)
         self.assertEqual(res.status_code, 201)
-        self.assertIn('Go to Borabora', str(res.data))
+        self.assertGreaterEqual(len(data['id']), 8)
+
+    def test_message_get(self):
+        res = self.client().post('/messages/username', data={'message': self.message_data})
+        data = json.loads(res.data)
+        raw_message = self.client().get('/messages/username/' + data['id'])
+        message = json.loads(raw_message.data)
+        self.assertEqual(raw_message.status_code, 200)
+        self.assertEqual(message['message'], self.message_data)
+
+    def test_message_get_not_found(self):
+        raw_message = self.client().get('/messages/username/bogusId')
+        self.assertEqual(raw_message.status_code, 404)
+
+    def test_message_delete(self):
+        res = self.client().post('/messages/username', data={'message': self.message_data})
+        data = json.loads(res.data)
+        raw_message = self.client().delete('/messages/username/' + data['id'])
+        self.assertEqual(raw_message.status_code, 204)
+
+    def test_message_delete_not_found(self):
+        raw_message = self.client().delete('/messages/username/bogusId')
+        self.assertEqual(raw_message.status_code, 404)
+
+    def test_get_messages(self):
+        m1 = json.loads(self.client().post('/messages/username', data={'message': self.message_data}).data)
+        m2 = json.loads(self.client().post('/messages/username', data={'message': self.message_data}).data)
+        ids = {m1.get('id'), m2.get('id')}
+        messages = json.loads(self.client().get('/messages/username').data)
+        filtered_res = [message for message in messages if message.get('id') in ids]
+        self.assertEqual(len(ids), len(filtered_res))
 
     def tearDown(self):
         """teardown all initialized variables."""
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
